3fd49604418ed9bd53984b33603de80ce0a44bd2
TSučević - Blade <32307645+tsucevic@users.noreply.github.com>
2020-10-21 10:28:23 +0200

    Added overload with out.

--- UtilitiesLib/Utilities.cs
+++ UtilitiesLib/Utilities.cs
@@ -1,9 +1,27 @@
 ﻿using System;
+using System.Xml.Linq;
 
 namespace UtilitiesLib
 {
     public class Utilities
     {
+        /// <summary>
+        /// Reads string from console and saves to variable.
+        /// </summary>
+        /// <param name="message">A message to print out to the user.</param>
+        /// <param name="outputVariable">Variable in which the string will be saved.</param>
+        /// <returns>User input.</returns>
+        public static string ReadString(string message, out string outputVariable)
+        {
+            do
+            {
+                Console.WriteLine(message);
+                outputVariable = Console.ReadLine().Trim(); // remove excess whitespaces
+            } while (string.IsNullOrWhiteSpace(outputVariable));
+
+            return outputVariable;
+        }
+
         /// <summary>
         /// Reads string from console.
         /// </summary>
@@ -11,14 +29,24 @@ namespace UtilitiesLib
         /// <returns>User input.</returns>
         public static string ReadString(string message)
         {
-            string s;
+            ReadString(message, out string s);
+            return s;
+        }
+
+        /// <summary>
+        /// Reads int from console.
+        /// </summary>
+        /// <param name="message">A message to print out to the user.</param>
+        /// <param name="outputVariable">Variable in which the int will be saved.</param>
+        /// <returns>User input.</returns>
+        public static int ReadInt(string message, out int outputVariable)
+        {
             do
             {
                 Console.WriteLine(message);
-                s = Console.ReadLine().Trim(); // remove excess whitespaces
-            } while (string.IsNullOrWhiteSpace(s));
+            } while (!int.TryParse(Console.ReadLine(), out outputVariable)); // check if it can be parsed into int and parse to n
 
-            return s;
+            return outputVariable;
         }
 
         /// <summary>
@@ -28,13 +56,24 @@ namespace UtilitiesLib
         /// <returns>User input.</returns>
         public static int ReadInt(string message)
         {
-            int n;
+            ReadInt(message, out int n);
+            return n;
+        }
+
+        /// <summary>
+        /// Reads double from console.
+        /// </summary>
+        /// <param name="message">A message to print out to the user.</param>
+        /// <param name="outputVariable">Variable in which the double will be saved.</param>
+        /// <returns>User input.</returns>
+        public static double ReadDouble(string message, out double outputVariable)
+        {
             do
             {
                 Console.WriteLine(message);
-            } while (!int.TryParse(Console.ReadLine(), out n)); // check if it can be parsed into int and parse to n
+            } while (!double.TryParse(Console.ReadLine(), out outputVariable)); // check if it can be parsed into double and parse to n
 
-            return n;
+            return outputVariable;
         }
 
         /// <summary>
@@ -44,12 +83,7 @@ namespace UtilitiesLib
         /// <returns>User input.</returns>
         public static double ReadDouble(string message)
         {
-            double n;
-            do
-            {
-                Console.WriteLine(message);
-            } while (!double.TryParse(Console.ReadLine(), out n)); // check if it can be parsed into double and parse to n
-
+            ReadDouble(message, out double n);
             return n;
         }
     }
